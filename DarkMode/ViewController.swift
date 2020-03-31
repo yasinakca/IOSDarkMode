71946683f1be194834138dab20570cc7fed935aa
YASIN AKCA <yasin.akca@bil.omu.edu.tr>
2020-03-31 13:34:26 +0300

    ios dark mode uygulamaları

--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //kullanıcının secimini ezmek istersem
+        //overrideUserInterfaceStyle = .light
+        
+        //butun app'de kullanıcının secimini ezmek icin info.plist dosyasında User Interface Style ayarı degistirilebilir.
+        
+        
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        }else {
+            changeButton.tintColor = UIColor.blue
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        } else {
+            changeButton.tintColor = UIColor.blue
+        }
     }
 
 
